918b5e603bc8afdf29e2ad044cb51ee3e44e51bc
VIGNESH99999 <99714405+VIGNESH99999@users.noreply.github.com>
2022-03-09 22:53:11 +0530

    Add files via upload

---
+++ TASK-8/qn 5th(2).py
@@ -0,0 +1,10 @@
+import numpy as VIGNESH
+
+in_1starray = VIGNESH.array([[2, -7, 5], [-6, 2, 0]])
+in_2ndarray = VIGNESH.array([[5, 8, -5], [3, 6, 9]])
+
+print("1st Input array : ", in_1starray)
+print("2nd Input array : ", in_2ndarray)
+
+out_arr = VIGNESH.add(in_1starray, in_2ndarray)
+print("output sum array : ", out_arr)
