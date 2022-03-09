1843aa8a6ce9b2e66c5ffaf9a4a8e8d0d8f01214
VIGNESH99999 <99714405+VIGNESH99999@users.noreply.github.com>
2022-03-09 22:49:44 +0530

    Add files via upload

---
+++ TASK-8/qn 5th(1).py
@@ -0,0 +1,10 @@
+import numpy as vignesh
+
+in_x = 10
+in_y = 15
+
+print("1st Input   : ", in_x)
+print("2nd Input   : ", in_y)
+
+out_num = vignesh.add(in_x, in_y)
+print("output number after addition  : ", out_num)
